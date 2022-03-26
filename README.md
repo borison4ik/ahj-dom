d0f4bdabbdc63c045a40b55090628e100ebe80bb
Valentin Borisenko <borison2004@yandex.ru>
2022-03-26 17:02:56 +0700

    resolved

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/a4e9cm8lxpxnijr2/branch/main?svg=true)](https://ci.appveyor.com/project/borison4ik/ahj-dom/branch/main)
+
+# DOM
+
+## Перемещение элемента
---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+import goblin from '../img/goblin.png';
+
+window.onload = () => {
+  const AREA = document.querySelector('.area');
+  const SIZE = 4;
+  const SPEED = 500;
+  let RND = 0;
+  let IMG_FACE = null;
+  let CELL = null;
+
+  drowMap();
+  animate();
+
+  // drow map
+  function drowMap() {
+    for (let i = 0; i < SIZE ** 2; i += 1) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.dataset.id = i;
+      AREA.appendChild(cell);
+    }
+  }
+  // drow Image
+  function drowImage() {
+    if(CELL) CELL.innerHTML = '';
+    IMG_FACE = new Image();
+    IMG_FACE.src = goblin;
+    RND = generateRandomNumber();
+    CELL = AREA.querySelector(`[data-id="${RND}"]`);
+    CELL.insertAdjacentElement('afterbegin', IMG_FACE);
+  }
+  // generate random number
+  function generateRandomNumber() {
+    let number = Math.floor(Math.random() * SIZE ** 2);
+    if(number === RND) {
+      do {
+        number = Math.floor(Math.random() * SIZE ** 2);
+      } while (number !== RND);
+    }
+    return number;
+  }
+  // animate
+  function animate() {
+    setTimeout(() => {
+      drowImage();
+      animate();
+    }, SPEED);
+  }
+};
